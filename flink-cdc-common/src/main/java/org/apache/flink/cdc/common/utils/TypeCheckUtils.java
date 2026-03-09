6ad63470906879225b3f6917ddc2e07491dabd3d
xxntti3n <thaitien0563621@gmail.com>
2026-03-09 15:28:20 +0700

    linter

--- flink-cdc-common/src/main/java/org/apache/flink/cdc/common/utils/TypeCheckUtils.java
+++ flink-cdc-common/src/main/java/org/apache/flink/cdc/common/utils/TypeCheckUtils.java
@@ -102,8 +102,7 @@ public class TypeCheckUtils {
         // ordered by type root definition
         switch (type.getTypeRoot()) {
             case CHAR:
-            case VARCHAR: // The internal representation of String is BinaryString which is
-            // mutable
+            case VARCHAR: // The internal representation of String is BinaryString which is mutable
             case ARRAY:
             case MAP:
             case ROW:
